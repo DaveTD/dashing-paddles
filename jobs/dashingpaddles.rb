3f0f8fa507b43b72e1a8710183f8ca6312a14c2b
DaveTD <its.dave.dawson@gmail.com>
2013-09-06 00:41:06 -0400

    Update dashingpaddles.rb

--- jobs/dashingpaddles.rb
+++ jobs/dashingpaddles.rb
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-
 
 SCHEDULER.every'10s', :first_in => 1 do |work|
 	work.unschedule
-=======
-SCHEDULER.every'10s', :first_in => 1 do |work|
-	work.unschedule
-	send_event('dashingpaddles', value: rand(100))
->>>>>>> aa96ad53c20cdc17e6707e9f7670da9dbf642414
 	y = Gameserver.new
 end
 
